fc6cc15cd6983fbb8c6fc62ac334d4b061038ee2
Ashish Pal <118504077+AshishPal8@users.noreply.github.com>
2024-06-25 18:17:15 +0530

    final features and how it works section

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,16 +2,12 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
 import VideoCarousel from "./VideoCarousel";
+import { animateWithGsap } from "../utils/animations";
 
 const Highlights = () => {
   useGSAP(() => {
-    gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to(".link", {
-      opacity: 1,
-      y: 0,
-      duration: 1,
-      stagger: 0.25,
-    });
+    animateWithGsap("#title", { y: 0, opacity: 1 });
+    animateWithGsap(".link", { y: 0, opacity: 1, duration: 1, stagger: 0.25 });
   }, []);
 
   return (
